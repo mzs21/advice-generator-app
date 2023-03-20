a8131823391324cbd78ff4b792bbe30194640d77
mzs21 <snigdho2111@gmail.com>
2023-03-20 11:45:26 +0600

    fixed: issues

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
-import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
   plugins: [react()],
